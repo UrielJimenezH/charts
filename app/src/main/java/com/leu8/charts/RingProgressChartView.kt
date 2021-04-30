310d2a2f3cad5a0d19fd76b5b03156c8e55efb0a
Uriel Jiménez Hernández <urieljh11@gmail.com>
2021-04-30 13:28:21 -0500

    RIng progress chart view, refreshDrawableState when progressPercentage or text is set

--- app/src/main/java/com/leu8/charts/RingProgressChartView.kt
+++ app/src/main/java/com/leu8/charts/RingProgressChartView.kt
@@ -14,12 +14,11 @@ class RingProgressChartView(context: Context, attrs: AttributeSet): View(context
     private val textPaint = Paint()
     private val oval: RectF = RectF()
 
-    private var canvas: Canvas? = null
-
     var progressPercentage: Int = 50
         set(value) {
             field = value
-            draw(canvas ?: return)
+
+            refreshDrawableState()
         }
     var barColor: Int = 0
     var barBackgroundColor: Int = 0
@@ -28,7 +27,7 @@ class RingProgressChartView(context: Context, attrs: AttributeSet): View(context
     var text: String = ""
         set(value) {
             field = value
-            draw(canvas ?: return)
+            refreshDrawableState()
         }
     var textSize: Float = 30f
     var textColor: Int = 0
@@ -93,8 +92,6 @@ class RingProgressChartView(context: Context, attrs: AttributeSet): View(context
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        if (this.canvas == null)
-            this.canvas = canvas
 
         val width = width.toFloat()
         val height = height.toFloat()
